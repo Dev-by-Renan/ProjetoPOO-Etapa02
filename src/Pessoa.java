3e43fea056e24b847e649640aaa5a6cbbb63e69d
DeVieira-Nobrega <felipe.dev.nobrega@gmail.com>
2026-06-28 20:11:42 -0300

    Update Pessoa.java
    modificação de atributos para privados, getters e setters atualizados com validação

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -6,7 +6,21 @@ abstract class Pessoa {
         return nome;
     }
     public void setNome(String nome) {
+    if (nome != null && !nome.equals("")) {
         this.nome = nome;
     }
-    public abstract void exibirResumo();
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+    if (cpf != null && !cpf.equals("")) {
+        this.cpf = cpf;
+    }
+    }
+        public Pessoa(String nome, String cpf) {
+    setNome(nome);
+    setCpf(cpf);
+}
+    public abstract String exibirResumo();
 }
