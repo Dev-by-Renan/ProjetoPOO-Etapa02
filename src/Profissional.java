dec4ce4ea1dc574d61bc5cdcc45436abfd74919c
Dev-by-Renan <rodriquesaraujo41@gmail.com>
2026-06-28 23:24:42 -0300

    Update Profissional.java
    Adicionado a herança entre pessoa e Profissional e o diagrama de classe e relacionamentos

--- src/Profissional.java
+++ src/Profissional.java
@@ -1,166 +1,182 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Profissional implements Exibivel {
+public class Profissional extends Pessoa implements Exibivel {
 
+    private String especialidade;
+    private String registroProfissional;
+    private double valorConsulta;
 
-//Fazendo o encapsulamento:
+    // Adicionando coleção
+    private List<String> diasDisponiveis;
 
-private String nome;
-private String especialidade;
-private String registroProfissional;
-private double valorConsulta;
+    // Sobrecargas:
+    public Profissional(String nome, String especialidade) {
 
-private List<String> diasDisponiveis;
+        super(nome);
 
-// Sequencia de sobrecarga com diferentes paramentros:
-
-public Profissional(String nome, String especialidade) {
-    this.nome = nome;
-    this.especialidade = especialidade;
-    this.registroProfissional = "";
-    this.valorConsulta = 0;
-    this.diasDisponiveis = new ArrayList<>();
-}
-
-public Profissional(String nome, String especialidade,
-                    String registroProfissional,
-                    double valorConsulta) {
-
-    this.nome = nome;
-    this.especialidade = especialidade;
-    this.registroProfissional = registroProfissional;
-    this.valorConsulta = valorConsulta;
-    this.diasDisponiveis = new ArrayList<>();
-}
-
-public Profissional(String nome, String especialidade,
-                    String registroProfissional,
-                    double valorConsulta,
-                    List<String> dias) {
-
-    this.nome = nome;
-    this.especialidade = especialidade;
-    this.registroProfissional = registroProfissional;
-    this.valorConsulta = valorConsulta;
-    this.diasDisponiveis = new ArrayList<>(dias);
-}
-
-public void atualizar(String registro, double valor) {
-    setRegistroProfissional(registro);
-    setValorConsulta(valor);
-}
-
-public void atualizar(String registro, double valor,
-                      List<String> dias) {
-
-    setRegistroProfissional(registro);
-    setValorConsulta(valor);
-    this.diasDisponiveis = new ArrayList<>(dias);
-}
-
-public boolean atendeNoDia(String dia) {
-    return diasDisponiveis.contains(dia);
-}
-
-public static boolean especialidadeValida(String esp) {
-    return esp.equals("clinica geral")
-            || esp.equals("fisioterapia")
-            || esp.equals("psicologia")
-            || esp.equals("nutricao");
-}
-
-
-protected String gerarIdentificacao() {
-    return nome + " - " + especialidade;
-}
-
-@Override
-public String exibirResumo() {
-
-    String dias = String.join(", ", diasDisponiveis);
-
-    return "Nome: " + nome
-            + " | Espec: " + especialidade
-            + " | Reg: " + registroProfissional
-            + " | Valor: R$" + valorConsulta
-            + " | Dias: " + dias;
-}
-
-public String getNome() {
-    return nome;
-}
-
-public String getEspecialidade() {
-    return especialidade;
-}
-
-public String getRegistroProfissional() {
-    return registroProfissional;
-}
-
-public double getValorConsulta() {
-    return valorConsulta;
-}
-
-public List<String> getDiasDisponiveis() {
-    return diasDisponiveis;
-}
-
-public void setNome(String nome) {
-
-    if (nome == null || nome.isBlank()) {
-        throw new IllegalArgumentException(
-                "Nome não pode ser vazio");
+        this.especialidade = especialidade;
+        this.registroProfissional = "";
+        this.valorConsulta = 0;
+        this.diasDisponiveis = new ArrayList<>();
     }
 
-    this.nome = nome;
-}
+    public Profissional(String nome, String especialidade,
+                        String registroProfissional,
+                        double valorConsulta) {
 
-public void setEspecialidade(String especialidade) {
+        super(nome);
 
-    if (!especialidadeValida(especialidade)) {
-        throw new IllegalArgumentException(
-                "Especialidade inválida");
+        this.especialidade = especialidade;
+        this.registroProfissional = registroProfissional;
+        this.valorConsulta = valorConsulta;
+        this.diasDisponiveis = new ArrayList<>();
     }
 
-    this.especialidade = especialidade;
-}
+    public Profissional(String nome, String especialidade,
+                        String registroProfissional,
+                        double valorConsulta,
+                        List<String> dias) {
 
-public void setRegistroProfissional(String registroProfissional) {
+        super(nome);
 
-    if (registroProfissional == null
-            || registroProfissional.isBlank()) {
-
-        throw new IllegalArgumentException(
-                "Registro profissional inválido");
+        this.especialidade = especialidade;
+        this.registroProfissional = registroProfissional;
+        this.valorConsulta = valorConsulta;
+        this.diasDisponiveis = new ArrayList<>(dias);
     }
 
-    this.registroProfissional = registroProfissional;
-}
+    public void atualizar(String registro, double valor) {
+        setRegistroProfissional(registro);
+        setValorConsulta(valor);
+    }
+    
+    public void atualizar(String registro, double valor,
+                          List<String> dias) {
 
-public void setValorConsulta(double valorConsulta) {
-
-    if (valorConsulta < 0) {
-        throw new IllegalArgumentException(
-                "Valor da consulta não pode ser negativo");
+        setRegistroProfissional(registro);
+        setValorConsulta(valor);
+        this.diasDisponiveis = new ArrayList<>(dias);
     }
 
-    this.valorConsulta = valorConsulta;
+    public boolean atendeNoDia(String dia) {
+        return diasDisponiveis.contains(dia);
+    }
+
+    public static boolean especialidadeValida(String esp) {
+
+        return esp.equals("clinica geral")
+                || esp.equals("fisioterapia")
+                || esp.equals("psicologia")
+                || esp.equals("nutricao");
+    }
+
+    // método protegido
+    protected String gerarIdentificacao() {
+        return getNome() + " - " + especialidade;
+    }
+
+    @Override
+    public String exibirResumo() {
+
+        String dias = String.join(", ", diasDisponiveis);
+
+        return "Nome: " + getNome()
+                + " | Espec: " + especialidade
+                + " | Reg: " + registroProfissional
+                + " | Valor: R$" + valorConsulta
+                + " | Dias: " + dias;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public String getRegistroProfissional() {
+        return registroProfissional;
+    }
+
+    public double getValorConsulta() {
+        return valorConsulta;
+    }
+
+    public List<String> getDiasDisponiveis() {
+        return diasDisponiveis;
+    }
+
+    public void setEspecialidade(String especialidade) {
+
+        if (!especialidadeValida(especialidade)) {
+            throw new IllegalArgumentException(
+                    "Especialidade inválida");
+        }
+
+        this.especialidade = especialidade;
+    }
+
+    public void setRegistroProfissional(String registroProfissional) {
+
+        if (registroProfissional == null
+                || registroProfissional.isBlank()) {
+
+            throw new IllegalArgumentException(
+                    "Registro profissional inválido");
+        }
+
+        this.registroProfissional = registroProfissional;
+    }
+
+    public void setValorConsulta(double valorConsulta) {
+
+        if (valorConsulta < 0) {
+            throw new IllegalArgumentException(
+                    "Valor da consulta não pode ser negativo");
+        }
+
+        this.valorConsulta = valorConsulta;
+    }
+
+    public void setDiasDisponiveis(List<String> diasDisponiveis) {
+        this.diasDisponiveis = new ArrayList<>(diasDisponiveis);
+    }
+
+
+    public void adicionarDiaDisponivel(String dia) {
+        diasDisponiveis.add(dia);
+    }
+
+    public String obterDia(int indice) {
+        return diasDisponiveis.get(indice);
+    }
 }
 
-public void setDiasDisponiveis(List<String> diasDisponiveis) {
-    this.diasDisponiveis = new ArrayList<>(diasDisponiveis);
-}
 
-// Operações de List 
+/*
+    Diagrama completo da classe:
+- especialidade : String
+- registroProfissional : String
+- valorConsulta : double
+- diasDisponiveis : List<String>
 
-public void adicionarDiaDisponivel(String dia) {
-    diasDisponiveis.add(dia);
-}
++ Profissional(nome, especialidade)
++ Profissional(nome, especialidade, registro, valor)
++ Profissional(nome, especialidade, registro, valor, dias)
 
-public String obterDia(int indice) {
-    return diasDisponiveis.get(indice);
-}
++ atualizar(registro, valor)
++ atualizar(registro, valor, dias)
 
-}
++ atendeNoDia(dia) : boolean
++ exibirResumo() : String
+
+# gerarIdentificacao() : String
+
+    (Relacionamentos)
+
+Pessoa -----------|> Profissional
+                     (HERANÇA)
+
+Profissional ......|> Exibivel
+                     (IMPLEMENTA INTERFACE)
+
+Profissional ------> List<String>
+                     (AGREGAÇÃO dos dias disponíveis) */
