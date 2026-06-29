3c123ce2125a56a94f2ded6e24c215b962bcbeaf
DeVieira-Nobrega <felipe.dev.nobrega@gmail.com>
2026-06-28 22:15:25 -0300

    Update Pessoa.java
    remoção de cpf como atributo de "Pessoa"

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,6 +1,5 @@
 abstract class Pessoa {
     private String nome;
-    private String cpf;
     
     public String getNome() {
         return nome;
@@ -10,17 +9,8 @@ abstract class Pessoa {
         this.nome = nome;
     }
     }
-    public String getCpf() {
-        return cpf;
-    }
-    public void setCpf(String cpf) {
-    if (cpf != null && !cpf.equals("")) {
-        this.cpf = cpf;
-    }
-    }
-        public Pessoa(String nome, String cpf) {
+        public Pessoa(String nome) {
     setNome(nome);
-    setCpf(cpf);
 }
     public abstract String exibirResumo();
 }
